0c68f5a6e9d2f2d153549765e3224ef75414ca19
Mahdi Jafari <mahdi.jfri.79@gmail.com>
2021-08-11 01:53:57 +0430

    calculate median of distances by binary search

---
+++ median_of_distances.h
@@ -0,0 +1,10 @@
+//
+// Created by mahdi on 11.08.21.
+//
+
+#ifndef NCFINAL_MEDIAN_OF_DISTANCES_H
+#define NCFINAL_MEDIAN_OF_DISTANCES_H
+
+double get_median_of_distances(int n);
+
+#endif //NCFINAL_MEDIAN_OF_DISTANCES_H
