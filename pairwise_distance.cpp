6b2c72edfe0884667fb4408b93cd2a0af42d2f7d
solale <seiedesolale@gmail.com>
2021-08-13 01:25:15 +0430

    fix vectors

--- pairwise_distance.cpp
+++ pairwise_distance.cpp
@@ -5,6 +5,8 @@
 #include<thread>
 #include <math.h>
 
+std::vector<std::vector<double>> M;
+
 double euclidean_distances(std::vector<double> x, std::vector<double> y) {
     double sum = 0;
     for (int i = 0; i < x.size(); ++i) {
@@ -13,14 +15,13 @@ double euclidean_distances(std::vector<double> x, std::vector<double> y) {
     return sum;
 }
 
-void calculate_bulk_distance(std::vector<std::vector<double>> X, std::vector<std::vector<double>> Y, int X_begin_index,
-                             std::string file_name) {
+void calculate_bulk_distance(int X_begin_index, int X_end_index, std::string file_name) {
     std::ofstream output_file("pairwise_distance/" + file_name + ".txt");
-    for (int i = 0; i < X.size(); ++i) {
-        for (int j = 0; j < Y.size(); ++j) {
+    for (int i = X_begin_index; i < X_end_index; ++i) {
+        for (int j = 0; j < M.size(); ++j) {
             double euclidean_distance;
-            euclidean_distance = euclidean_distances(X[i], Y[j]);
-            output_file << i + X_begin_index << " " << j << " " << euclidean_distance << '\n';
+            euclidean_distance = euclidean_distances(M[i], M[j]);
+            output_file << i << " " << j << " " << euclidean_distance << '\n';
         }
     }
     output_file.close();
@@ -28,21 +29,19 @@ void calculate_bulk_distance(std::vector<std::vector<double>> X, std::vector<std
 }
 
 
-void pairwise_distance(std::vector<std::vector<double>> X, std::vector<std::vector<double>> Y) {
-    int bulk_count = 8;
-    int bulk_size = X.size() / bulk_count;
+void pairwise_distance() {
+    int bulk_count = 1;
+    int bulk_size = M.size() / bulk_count;
     if (bulk_size == 0)
-        bulk_size = X.size();
+        bulk_size = M.size();
     int offset = 0;
     std::vector<std::thread> v;
-    while (offset < X.size()) {
-        auto first = X.begin() + offset;
-        auto last = X.begin() + std::min(offset + bulk_size, (int) X.size());;
-        std::vector<std::vector<double>> newVec(first, last);
+    while (offset < M.size()) {
         char I = '0' + offset / bulk_size;
         std::string file_name;
         file_name.push_back(I);
-        std::thread thread_obj(calculate_bulk_distance, newVec, Y, offset, file_name);
+        std::thread thread_obj(calculate_bulk_distance, offset, std::min(offset + bulk_size, (int) M.size()),
+                               file_name);
         v.push_back(move(thread_obj));
         offset += bulk_size;
     }
